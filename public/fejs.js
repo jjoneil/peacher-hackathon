9785d726e38402f5b0a6eedab701230e4c5facd5
achin4bacon <kendra.leigh.cooper@gmail.com>
2017-03-01 09:49:26 -0700

    added JS and stuffs

--- public/fejs.js
+++ public/fejs.js
@@ -1,2 +1,36 @@
 $('#myModal').modal('show');
-// www.jquery2dotnet.com
+
+
+$('log').click(function(){
+    var isTeacher = $('#isTeacher').is(":checked");
+    var isParent = $('#isParent').is(":checked");
+    $.post('/login', {
+        username: $('#username').val(),
+        password: $('#password').val(),
+        student: isParent
+    }, function(res){
+        //change if statements according to res
+        if(isTeacher === true){
+            document.location = './teacherprof/:id'
+        }
+        if(isParent === true){
+            document.location = './studentprof/:id'
+        }
+    });
+});
+
+
+$('#register').click(function(){
+    var teachorparent = $('#teachorparent').is(":checked");
+    $.post('/signup',{
+        username: $('usernamesignup').val(), 
+        password: $('passwordsignup').val()
+    }, function(res){
+        if(teachorparent === true){
+            document.location = './teacherprof'
+        }
+        else if(teachorparent === false){
+            document.location = './parentprof'
+        }
+    });
+});
